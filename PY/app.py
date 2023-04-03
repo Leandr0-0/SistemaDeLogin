fe974bd8cc820069011e027f4d56abfb99446d2c
Leandro Silva <sr.syllw@gmail.com>
2023-04-03 11:28:57 -0300

    Add if e elif

--- PY/app.py
+++ PY/app.py
@@ -8,19 +8,37 @@ o sistema deve alertar caos o loguns ou a senha não estejam corretos'''
 # O sistema deve alerta caso o login ou a senha não estejam corretos;
 # Criar um banco de dados.
 
+# Permitindo que o usuário cadastrados façam login
+
+resposta = input('[1] Cadastra novo usuário [2] Fazer login: ')
+
 # Armazenado os usuários e existentes:
 usuarios = ['carol', 'amanda', 'jeff']
 senhas = ['12345', 'abcde', '123abc']
 
-# Recebendo um usuario:
-usuario_digitado = input('Digite seu usuário: ')
+if resposta =='1':
+    # Recebendo um usuario:
+    usuario_digitado = input('Digite seu usuário: ')
 
-# Recebendo uma senha:
-senha_digitada = input('Digite sua senha: ')
+    # Recebendo uma senha:
+    senha_digitada = input('Digite sua senha: ')
 
-# Caso não exista, cadastrar usuário e senha:
-usuarios.append(usuario_digitado)
-senhas.append(senha_digitada)
+    # Caso não exista, cadastrar usuário e senha:
+    usuarios.append(usuario_digitado)
+    senhas.append(senha_digitada)
 
-# confirmar que funcionou
-print(usuarios)
+    # Confirmar que funcionou
+    print(usuarios)
+
+# Permitir que usuários já cadastrados façam login 
+elif resposta == '2':
+    # Permitir que usuários já cadastrados possam fazer login
+
+    # Pedir usuário
+    usuario_digitado = input('Digite seu usuário: ')
+    # Pedir senha
+    senha_digitada = input('digite sua senha: ')
+    # Verificar se o usuário está na lista
+    if usuario_digitado in usuarios:
+        print('Encontrado!')
+    # Verificar se a senha fornecida pelo usuário e a mesma da lista
